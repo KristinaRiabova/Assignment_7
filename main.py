f52c6acc5d140063c91be42be2cca0e8f632a028
Kristina <kristinaer304@gmail.com>
2022-12-09 21:56:23 +0200

    Напишіть програму, що приймає наступні аргументи командного рядка:
    Адреса файлу з даними;
    Другим аргументом - завжди -medals
    Країна. Назва може вводитись як за повною назвою (колонка Team), так і за кодом
    Рік проведення Олімпіади
    (Опціонально) -output
    (Опціонально) файл для виведення результатів

--- main.py
+++ main.py
@@ -1,2 +1,24 @@
+import argparse
 
+parser = argparse.ArgumentParser(description="Hello")
+parser.add_argument("path", type=str)
+parser.add_argument("medals",type=str, default="-medals")
+parser.add_argument("country", type=str)
+parser.add_argument("year", type=int)
+parser.add_argument("-output", type=str)
+args = parser.parse_args()
+path = args.path
+country = args.country
+year = args.year
+print(args)
+
+with open(path, 'r') as file:
+    lines = file.readlines()
+    lineForSplit = ""
+    newLines = []
+    for i in range(0, len(lines)):
+        lineForSplit = str(lines[i].split('\t'))
+        newLines.append(lineForSplit)
+
+print(newLines)
 
