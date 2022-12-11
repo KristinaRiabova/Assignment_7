84d5cb666637f5908c81303058d430ebe09c726e
Kristina <kristinaer304@gmail.com>
2022-12-12 01:34:37 +0200

    Завдання 1
    Програма має обробити наданий файл та вивести в стовбчик перші десять медалістів з цієї країни на заданій олімпіаді (у форматі "ім'я - дисципліна - тип медалі"), а під цим - сумарну кількість медалей за типом (золоті, срібні та бронзові) у цієї країни. Програма має коректно обробляти крайні випадки (у заданої країни менше 10 медалей, введена країна не існує або у введений рік не проводилась олімпіада). Якщо користувач ввів параметр -output, то результат має бути виведений не тільки на екран, а і у вказаний файл

--- main.py
+++ main.py
@@ -4,21 +4,82 @@ parser = argparse.ArgumentParser(description="Hello")
 parser.add_argument("path", type=str)
 parser.add_argument("medals",type=str, default="-medals")
 parser.add_argument("country", type=str)
-parser.add_argument("year", type=int)
+parser.add_argument("year", type=str)
 parser.add_argument("-output", type=str)
 args = parser.parse_args()
 path = args.path
 country = args.country
 year = args.year
-print(args)
+pathOutput = args.output
+
+def countMedals(medalsList):
+    gold = 0
+    bronze = 0
+    silver = 0
+    for medal in medalsList:
+        if medal == 'Gold':
+            gold += 1
+        elif medal == 'Silver':
+            silver += 1
+        elif medal == 'Bronze':
+            bronze += 1
+
+    return gold+silver+bronze
+
+
+names = []
+countAthletes = 0
+medals = []
+yearsOlymp = []
+
+for i in range(1896,2016,4):
+    yearsOlymp.append(i)
+
+if (pathOutput != None):
+    fileOutput = open(pathOutput,"w")
 
 with open(path, 'r') as file:
-    lines = file.readlines()
-    lineForSplit = ""
-    newLines = []
-    for i in range(0, len(lines)):
-        lineForSplit = str(lines[i].split('\t'))
-        newLines.append(lineForSplit)
+    file.readline()
+    nextLine = file.readline()
+    while nextLine:
+        if int(year) not in yearsOlymp:
+            print('No olymp this year')
+            nextLine = file.readline()
+            quit()
+        splitLine = nextLine.split('\t')
+        medalLine = splitLine[-1][:-1]
+        nameAthlete = splitLine[1]
+        countryAthlete = splitLine[-9].split('/')
+        sportAthlete = splitLine[-3]
+        if country in countryAthlete or country in splitLine:
+            if year in splitLine:
+                while countAthletes < 10:
+                    if nameAthlete not in names and medalLine != 'NA':
+                        if (pathOutput != None):
+                                fileOutput.write(f'{nameAthlete} - {sportAthlete} - {medalLine} \n')
+                        print(nameAthlete,"-",sportAthlete,"-",medalLine)
+                        countAthletes += 1
+                        names.append(nameAthlete)
+                    else:
+                        break
 
-print(newLines)
+                medals.append(medalLine)
 
+        nextLine = file.readline()
+
+    if len(names) == 0:
+        if (pathOutput != None):
+                fileOutput.write('invalid country')
+        print('invalid country')
+        quit()
+    if len(medals) < 10:
+        if (pathOutput != None):
+                fileOutput.write(f'that year {country} had less than 10 medals \n')
+        print('that year',country,'had less than 10 medals')
+
+if (pathOutput != None):
+        fileOutput.write(f'Summary medals : {countMedals(medals)} \n')
+if (pathOutput != None):
+    fileOutput.close()
+
+print('Summary medals : ',countMedals(medals))
