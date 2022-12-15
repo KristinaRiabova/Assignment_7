18020b583f8580936dde126f0311d5684bd1d89d
Kristina <kristinaer304@gmail.com>
2022-12-16 00:35:59 +0200

    Fixed #1 #2 add -total

--- main.py
+++ main.py
@@ -1,22 +1,62 @@
 import argparse
 
-parser = argparse.ArgumentParser(description="Hello")
-parser.add_argument("path", type=str)
-parser.add_argument("medals",type=str, default="-medals")
-parser.add_argument("country", type=str)
-parser.add_argument("year", type=str)
-parser.add_argument("-output", type=str)
+parser = argparse.ArgumentParser()
+parser.add_argument("--path")
+parser.add_argument("--mode")
+parser.add_argument("--country", required=False)
+parser.add_argument("--year")
+parser.add_argument("--output", required=False)
 args = parser.parse_args()
 path = args.path
+mode = args.mode
 country = args.country
 year = args.year
 pathOutput = args.output
 
-def countMedals(medalsList):
+#python main.py --path data.tsv --mode medals --country USA --year 1972
+#python main.py --path data.tsv --mode total --year 1972
+
+medals = []
+
+if (pathOutput != None):
+    fileOutput = open(pathOutput, "w")
+
+
+def countMedals(medals):
     gold = 0
     bronze = 0
     silver = 0
-    for medal in medalsList:
+    countAthletes = 0
+    with open(path, 'r') as file:
+        file.readline()
+        nextLine = file.readline()
+        while nextLine:
+            if int(year) not in yearsOlymp:
+                print('No olymp this year')
+                nextLine = file.readline()
+                quit()
+            splitLine = nextLine.split('\t')
+            medalLine = splitLine[-1][:-1]
+            nameAthlete = splitLine[1]
+            countryAthlete = splitLine[-9]
+            sportAthlete = splitLine[-3]
+            if country in countryAthlete or country in splitLine:
+                if year in splitLine:
+                    while countAthletes < 10:
+                        if nameAthlete not in names and medalLine != 'NA':
+                            if (pathOutput != None):
+                                fileOutput.write(f'{nameAthlete} - {sportAthlete} - {medalLine} \n')
+                            print(nameAthlete, "-", sportAthlete, "-", medalLine)
+                            countAthletes += 1
+                            names.append(nameAthlete)
+                        else:
+                            break
+
+                    medals.append(medalLine)
+
+            nextLine = file.readline()
+
+    for medal in medals:
         if medal == 'Gold':
             gold += 1
         elif medal == 'Silver':
@@ -24,62 +64,72 @@ def countMedals(medalsList):
         elif medal == 'Bronze':
             bronze += 1
 
-    return gold+silver+bronze
+    if len(names) == 0:
+        if (pathOutput != None):
+            fileOutput.write('invalid country')
+        print('invalid country')
+        quit()
+
+    if len(medals) < 10:
+        if (pathOutput != None):
+            fileOutput.write(f'that year {country} had less than 10 medals \n')
+        print('that year', country, 'had less than 10 medals')
+
+    return print(
+        f'{gold} gold medals, {silver} silver medals, {bronze} bronze medals, total : {gold + bronze + silver}')
+
+
+def total_medals(year):
+    if int(year) not in yearsOlymp:
+        print('No olymp this year')
+        quit()
+    dict = {
+
+    }
+    with open(path, 'r') as file:
+        file.readline()
+        line = file.readline()
+        while line:
+            splitLine = line.split('\t')
+            if year == splitLine[9]:
+                if splitLine[-1][:-1] != "NA":
+                    key = splitLine[-9]
+                    dict.setdefault(key, [])
+                    if len(dict[key]) == 0:
+                        dict[key].append(0)
+                        dict[key].append(0)
+                        dict[key].append(0)
+                    medal = splitLine[-1][:-1].lower()
+                    if medal == "gold":
+                        dict[key][0] += 1
+                    elif medal == "silver":
+                        dict[key][1] += 1
+                    elif medal == "bronze":
+                        dict[key][2] += 1
+
+            line = file.readline()
+
+
+        for key in dict:
+            print(key, " - ", dict[key][0], " - ", dict[key][1], " - ", dict[key][2])
+
+    return dict
 
 
 names = []
 countAthletes = 0
-medals = []
 yearsOlymp = []
 
-for i in range(1896,2016,4):
+for i in range(1896, 2016, 4):
     yearsOlymp.append(i)
 
-if (pathOutput != None):
-    fileOutput = open(pathOutput,"w")
 
-with open(path, 'r') as file:
-    file.readline()
-    nextLine = file.readline()
-    while nextLine:
-        if int(year) not in yearsOlymp:
-            print('No olymp this year')
-            nextLine = file.readline()
-            quit()
-        splitLine = nextLine.split('\t')
-        medalLine = splitLine[-1][:-1]
-        nameAthlete = splitLine[1]
-        countryAthlete = splitLine[-9].split('/')
-        sportAthlete = splitLine[-3]
-        if country in countryAthlete or country in splitLine:
-            if year in splitLine:
-                while countAthletes < 10:
-                    if nameAthlete not in names and medalLine != 'NA':
-                        if (pathOutput != None):
-                                fileOutput.write(f'{nameAthlete} - {sportAthlete} - {medalLine} \n')
-                        print(nameAthlete,"-",sportAthlete,"-",medalLine)
-                        countAthletes += 1
-                        names.append(nameAthlete)
-                    else:
-                        break
-
-                medals.append(medalLine)
-
-        nextLine = file.readline()
-
-    if len(names) == 0:
-        if (pathOutput != None):
-                fileOutput.write('invalid country')
-        print('invalid country')
-        quit()
-    if len(medals) < 10:
-        if (pathOutput != None):
-                fileOutput.write(f'that year {country} had less than 10 medals \n')
-        print('that year',country,'had less than 10 medals')
+if mode == 'total':
+    total_medals(year)
+else:
+    countMedals(medals)
 
 if (pathOutput != None):
-        fileOutput.write(f'Summary medals : {countMedals(medals)} \n')
+    fileOutput.write(f'Summary medals : {countMedals(medals)} \n')
 if (pathOutput != None):
     fileOutput.close()
-
-print('Summary medals : ',countMedals(medals))
